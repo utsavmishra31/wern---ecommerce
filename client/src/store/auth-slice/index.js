36a10f0a922b5408e4aeae36941e779914dfa283
Utsav Mishra <utsavmishra1979@gmail.com>
2025-05-20 16:45:55 +0530

    setitem to setItem

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -111,7 +111,7 @@ const authSlice = createSlice({
         state.user = action.payload.success ? action.payload.user : null;
         state.isAuthenticated = action.payload.success;
         state.token = action.payload.token;
-        sessionStorage.setitem('token', JSON.stringify(action.payload.token))
+        sessionStorage.setItem('token', JSON.stringify(action.payload.token))
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
