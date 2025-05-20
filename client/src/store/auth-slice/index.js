9a79f122de0ce922ff120b029dfc12603e85d62a
Utsav Mishra <utsavmishra1979@gmail.com>
2025-05-20 16:36:35 +0530

    change cookie logic to seesion storage

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -62,8 +62,9 @@ export const checkAuth = createAsyncThunk(
     const response = await axios.get(
       `${import.meta.env.VITE_API_URL}/api/auth/check-auth`,
       {
-        withCredentials: true,
+      
         headers: {
+          Authorization : `Bearer ${token}`,
           "Cache-Control":
             "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
@@ -79,6 +80,11 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action) => {},
+    resetTokenAndCredentials :(state)=>{
+    state.isAuthenticated = false;
+    state.user = null
+    state.token = null
+    }
   },
   extraReducers: (builder) => {
     builder
@@ -104,11 +110,14 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.user = action.payload.success ? action.payload.user : null;
         state.isAuthenticated = action.payload.success;
+        state.token = action.payload.token;
+        sessionStorage.setitem('token', JSON.stringify(action.payload.token))
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
+        state.token= null;
       })
       .addCase(checkAuth.pending, (state) => {
         state.isLoading = true;
@@ -131,5 +140,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { setUser } = authSlice.actions;
+export const { setUser, resetTokenAndCredentials } = authSlice.actions;
 export default authSlice.reducer;
--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -77,16 +77,29 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
-      success: true,
-      message: "Logged in successfully",
-      user: {
-        email: checkUser.email,
-        role: checkUser.role,
-        id: checkUser._id,
-        userName: checkUser.userName,
-      },
-    });
+   // res.cookie("token", token, { httpOnly: true, secure: true }).json({
+   //   success: true,
+   //   message: "Logged in successfully",
+    //  user: {
+    //    email: checkUser.email,
+    //    role: checkUser.role,
+    //    id: checkUser._id,
+    //    userName: checkUser.userName,
+    //  },
+   // });
+
+   res.status(200).json({
+    success: true,
+    message: "Logged in Successfully",
+    token,
+    user: {
+       email: checkUser.email,
+       role: checkUser.role,
+       id: checkUser._id,
+       userName: checkUser.userName,
+     },
+   })
+
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -106,8 +119,29 @@ const logoutUser = (req, res) => {
 };
 
 //auth middleware
+// const authMiddleware = async (req, res, next) => {
+//   const token = req.cookies.token;
+//   if (!token)
+//     return res.status(401).json({
+//       success: false,
+//       message: "Unauthorised user!",
+//     });
+
+//   try {
+//     const decoded = jwt.verify(token, "CLIENT_SECRET_KEY");
+//     req.user = decoded;
+//     next();
+//   } catch (error) {
+//     res.status(401).json({
+//       success: false,
+//       message: "Unauthorised user!",
+//     });
+//   }
+// };
+
 const authMiddleware = async (req, res, next) => {
-  const token = req.cookies.token;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]
   if (!token)
     return res.status(401).json({
       success: false,
